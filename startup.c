7bb9b1cc9a6148b701d8083f03f1a84dc10d6c76
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-03-29 21:40:23 +0300

    add systick to vector entry

--- startup.c
+++ startup.c
@@ -1,14 +1,29 @@
 #include <stdint.h>
 
 void Reset_Handler(void);
+void SysTick_Handler(void);
 
 extern uint32_t _stack_top, _data_flash_start, _data_start, _data_end, _bss_start, _bss_end;
 extern void main(void);
 
-const void * const _Vectors[] __attribute__((section(".isr_vector"))) = 
+const void * const _Vectors[] __attribute__((section(".isr_vector"))) =
 {
-    &_stack_top,
-    Reset_Handler 
+    &_stack_top,       
+    Reset_Handler,    
+    0,                 
+    0,               
+    0,                 
+    0,                 
+    0,              
+    0,                 
+    0,               
+    0,                 
+    0,                 
+    0,                
+    0,                
+    0,            
+    0,               
+    SysTick_Handler,   
 };
 
 void Reset_Handler(void)
