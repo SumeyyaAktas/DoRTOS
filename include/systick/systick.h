62a58398b8fa13042d4885bbe3c61f1eb20be68f
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-03-30 19:51:40 +0300

    set up syst addresses and syst init

---
+++ include/systick/systick.h
@@ -0,0 +1,18 @@
+#ifndef SYSTICK_H
+#define SYSTICK_H
+
+#include <stdint.h>
+
+#define SYST_CSR 0xE000E010
+#define SYST_RVR 0xE000E014
+#define SYST_CVR 0xE000E018
+
+#define CSR_ENABLE (1 << 0)
+#define CSR_TICKINT (1 << 1)
+#define CSR_CLKSOURCE (1 << 2)
+
+void systick_init(void);
+
+extern volatile uint32_t global_tick_counter;
+
+#endif
