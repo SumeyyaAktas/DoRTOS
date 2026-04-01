9bc565895327ead879d5313da4cca5ef30692894
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-01 09:34:30 +0300

    write tick loop

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "uart/pl011.h"
+#include "systick/systick.h"
 
 void hcf(void)
 {
@@ -12,6 +13,23 @@ void main(void)
 {
     uart_init();
     uart_puts("\nHello from TinyAbyss!\n");
-    
+
+    systick_init();
+    __asm__ volatile("cpsie i");
+
+    uint32_t last_tick = 0;
+
+    while (1)
+    {
+        if (global_tick_counter != last_tick)
+        {
+            last_tick = global_tick_counter;
+
+            uart_puts("Tick: ");
+            uart_putc('0' + (last_tick % 10)); 
+            uart_puts("\n");
+        }
+    }
+
     hcf();
 }
