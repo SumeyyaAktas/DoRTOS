6912543a93c826f16849854a26c418e507a454c0
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-04-03 12:22:06 +0300

    set up task and states

---
+++ include/task/task.h
@@ -0,0 +1,22 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <stdint.h>
+
+typedef enum 
+{
+    READY,
+    RUNNING,
+    BLOCKED
+} States;
+
+typedef struct 
+{
+    uint32_t *stack_pointer;
+    void (*entry)(void);
+    uint32_t stack[256];
+    uint8_t task_id;
+    States task_state;
+} TCB_t;
+
+#endif 
